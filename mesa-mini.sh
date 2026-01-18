76afe87ee39559981e90f0d9defe63bf02ab6d38
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-18 09:45:12 -0400

    fix nasty error

--- mesa-mini.sh
+++ mesa-mini.sh
@@ -67,7 +67,7 @@ rm -fv ./*-docs-*.pkg.tar.* ./*-debug-*.pkg.tar.*
 mv -v ./mesa-*.pkg.tar."$EXT"           ../mesa-mini-"$ARCH".pkg.tar."$EXT"
 mv -v ./vulkan-radeon-*.pkg.tar."$EXT"  ../vulkan-radeon-mini-"$ARCH".pkg.tar."$EXT"
 mv -v ./vulkan-nouveau-*.pkg.tar."$EXT" ../vulkan-nouveau-mini-"$ARCH".pkg.tar."$EXT"
-mv -v ./vulkan-virtio-*.pkg.tar."$EXT"  ../vulkan-virtio-nano-"$ARCH".pkg.tar."$EXT"
+mv -v ./vulkan-virtio-*.pkg.tar."$EXT"  ../vulkan-virtio-mini-"$ARCH".pkg.tar."$EXT"
 
 if [ "$ARCH" = 'x86_64' ]; then
 	mv -v ./vulkan-intel-*.pkg.tar."$EXT" ../vulkan-intel-mini-"$ARCH".pkg.tar."$EXT"
