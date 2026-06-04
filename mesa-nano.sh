6448168ad11bb3aced7e7a8573bc3930ad16b90d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-04 13:31:01 -0400

    add gallium `svga` (#66)

--- mesa-nano.sh
+++ mesa-nano.sh
@@ -7,7 +7,7 @@ sed -i -e 's|-O2|-Os|' /etc/makepkg.conf
 get-pkgbuild
 cd "$BUILD_DIR"
 
-common_gallium='d3d12,nouveau,radeonsi,softpipe,virgl,zink'
+common_gallium='d3d12,nouveau,radeonsi,softpipe,svga,virgl,zink'
 x64_gallium="crocus,iris,r600,$common_gallium"
 arm_gallium="asahi,freedreno,etnaviv,lima,panfrost,rocket,v3d,vc4,$common_gallium"
 
