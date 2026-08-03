9790d088e347741df898739b503d428d009b6ff2
Samueru-sama <xdglawyer@outlook.com>
2026-08-03 16:04:26 -0400

    build glycin-ng with upstream's PKGBUILD

--- glycin-mini.sh
+++ glycin-mini.sh
@@ -4,7 +4,7 @@ set -e
 
 sed -i -e 's|-O2|-Os|' /etc/makepkg.conf
 
-export AUR_PACKAGE=glycin-ng
+export PKGBUILD_REPO=https://github.com/QaidVoid/glycin-ng.git
 
 get-pkgbuild
 cd "$BUILD_DIR"
@@ -15,6 +15,6 @@ makepkg -fs --noconfirm --skippgpcheck
 
 ls -la
 rm -fv ./*-docs-*.pkg.tar.* ./*-debug-*.pkg.tar.* ./*-demos-*.pkg.tar.*
-mv -v ./"$AUR_PACKAGE"-*.pkg.tar."$EXT" ../"$PACKAGE"-mini-"$ARCH".pkg.tar."$EXT"
+mv -v ./glycin-ng-*.pkg.tar."$EXT" ../"$PACKAGE"-mini-"$ARCH".pkg.tar."$EXT"
 
 echo "All done!"
